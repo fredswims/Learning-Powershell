71b5c9b06bbee9ba3e0779285631537132e217ba
Fred Jacobowitz <fredswims@gmail.com>
2019-04-04 02:11:37 -0400

    Load on remote

--- MyStuff/AccessChampsEligibleV2.ps1
+++ MyStuff/AccessChampsEligibleV2.ps1
@@ -1,6 +1,6 @@
 $Version="2019-03-28.0"
 "Version $Version"
-"add to remote repository"
+"add to remote repository."
 
 #Added code in MakeObjects to flip the event to the heat.
 #This occurs in freestyle relays were the swimmer was leg 1

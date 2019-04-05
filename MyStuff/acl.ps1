74fb6fed528a1d2ea73afd6d34d76e1eafa08a7f
Fred Jacobowitz <fredswims@gmail.com>
2019-04-05 00:29:45 -0400

    Saturday

--- MyStuff/acl.ps1
+++ MyStuff/acl.ps1
@@ -1,4 +1,4 @@
-                           
+"Saturday"                           
 $path=C:\Users\freds_000\OneDrive\Documents\WindowsPowerShell #define path to the shared folder
 $reportpath =join-path $env:homepath ACL.csv #define path to export permissions report
 #script scans for directories under shared folder and gets acl(permissions) for all of them

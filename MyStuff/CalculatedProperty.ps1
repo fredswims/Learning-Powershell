b453d28321a8f6dab75ee0add15a99721365920d
Fred Swims <fredswims@gmail.com>
2019-05-12 20:10:26 -0400

    FredwasHere

--- MyStuff/CalculatedProperty.ps1
+++ MyStuff/CalculatedProperty.ps1
@@ -3,7 +3,9 @@
 https://powershell.org/2013/04/29/name-that-property/
 #>
 Get-ChildItem  | select-object -property LastWriteTime, @{name = "MyLabel"; expression = {$_.lastwritetime.toUniversalTime()}}
-
 Get-ChildItem  | select-object -property LastWriteTime, @{n = "MyLabel"; e = {$_.lastwritetime.toUniversalTime()}}
-
+#can use 'label' or '1' instead of 'name'
+#alternately
+$Whatever=@{n = "MyLabel"; e = {$_.lastwritetime.toUniversalTime()}}
+Get-ChildItem  | select-object -property LastWriteTime, $Whatever
 get-process | Sort-Object -Property BasePriority | Format-Table -GroupBy basepriority -wrap

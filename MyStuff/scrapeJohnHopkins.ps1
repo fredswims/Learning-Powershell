b305bbde4aed58c5aba3e14dea0e73a46f44b6e7
Fred Jacobowitz <fredswims@gmail.com>
2020-04-05 19:56:15 -0400

    working

--- MyStuff/scrapeJohnHopkins.ps1
+++ MyStuff/scrapeJohnHopkins.ps1
@@ -1,5 +1,5 @@
 #https://paullimblog.wordpress.com/2017/08/08/ps-tip-parsing-html-from-a-local-file-or-a-string/
-function Positives {
+function Covid19 {
 $StatesHere=@'
 Alabama
 Alaska
@@ -53,6 +53,76 @@ Wisconsin
 Wyoming
 '@
 $States = $($StatesHere -split "`n").TrimEnd("`r")
+set-location $env:OneDrive\PowershellScripts\MyStuff\Data\
+$path="./feature-list.html"
+
+$html = New-Object -ComObject "HTMLFile"
+$html.IHTMLDocument2_write($(Get-Content -path $path -raw))
+
+$Innertext=$html.all.tags("P")|Select-Object innertext
+#$ellen=new-object system.collections.arrayList
+#for($i=0;$i -lt $innertext.count;$i=$i+2){$ellen.add("{0}  {1}" -f $innertext[$i].innertext, $innertext[$i+1].innertext)}
+#for($i=0;$i -lt $innertext.count;$i=$i+2){$ellen.add($innertext[$i].innertext + ' ' + $innertext[$i+1].innertext)}
+#Process two at a time.
+
+for($i=0;$i -lt $innertext.count;$i+=2){
+    #$LineArray=$innertext[$i+1].innertext -split(' ')
+    $vLine=""
+    $IndexTemp=0
+    $Index=0
+    $Country=""
+    $State=""
+    $City=""
+    #write-host "$($innertext[$i].innertext):$($innertext[$i+1].innertext)"
+    $thisString=$innertext[$i+1].innertext
+    $Country=$thisstring.Substring($thisstring.LastIndexOf(" ")+1)
+    $vLine=$thisstring.Substring(0,$thisstring.LastIndexOf(" "))
+    foreach($aState in $States) {
+        $IndexTemp=$vline.LastIndexOf($aState)
+        if ($IndexTemp -gt 0){
+            $Index=$IndexTemp
+            $State=$aState
+            $City=$vline.Substring(0,$index-1)
+        }
+    } 
+    $thisString1=$innertext[$i].innertext
+    $index=$thisString1.LastIndexOf(" ")
+
+    #$innertext[$i].innertext
+    #">{0}<>{1}<>{2}<" -f $vline,$City,$State
+    $thisobject=   [pscustomobject]@{
+        oCount=[long]$innertext[$i].innertext.Substring(0,$index)
+        oStatus=$innertext[$i].innertext.Substring($index+1)
+        oCity=$City
+        oState=$State
+        oCountry=$Country
+    }
+    $thisobject
+}
+}#end of function
+
+
+
+
+# $object=$ellen|convertFrom-String -TemplateContent $template
+
+# $html.all.tags("P")|Select-Object innertext|ConvertFrom-String -TemplateContent $template
+
+
+# $html.all.tags("A")| foreach-object innertext
+# $html.all.tags("A")| out-file "covid.txt"
+# $html.all|out-file "covid.txt"
+# $html.all.tags("A")| select-object *
+# $html.all.tags("external-html")| where-object innertext -like "*new york city*"
+
+# $a=($html.allElements|Where-Object {$_.class -match "feature-list"}).innerText
+# $a=($html.allElements|Where-Object {$_.class -match "external-html"}).innerText
+
+# $tags=$html.all.tags("")
+
+# $url="https://coronavirus.jhu.edu/map.html"
+# $response = Invoke-WebRequest -Uri $url
+# $response.ParsedHtml.body.getelementsbyclassname('external-html')
 
 # $template=@'
 # {NAME:57,159 confirmed}
@@ -73,53 +143,3 @@ $States = $($StatesHere -split "`n").TrimEnd("`r")
 # $template=@'
 # {[long]Positive:57,159} {[string]Status:confirmed} {[string]City:New York City} {[string]State:New York} {[string]Country:US}
 # '@
-
-set-location $env:Homepath/mystuff/data
-$path="./feature-list.html"
-
-$html = New-Object -ComObject "HTMLFile"
-$html.IHTMLDocument2_write($(Get-Content -path $path -raw))
-
-$innertext=$html.all.tags("P")|Select-Object innertext
-$ellen=new-object system.collections.arrayList
-#for($i=0;$i -lt $innertext.count;$i=$i+2){$ellen.add("{0}  {1}" -f $innertext[$i].innertext, $innertext[$i+1].innertext)}
-#for($i=0;$i -lt $innertext.count;$i=$i+2){$ellen.add($innertext[$i].innertext + ' ' + $innertext[$i+1].innertext)}
-#Process two at a time.
-
-for($i=0;$i -lt $innertext.count;$i+=2){
-    $LineArray=$innertext[$i+1].innertext -split(' ')
-
-$thisobject=   [pscustomobject]@{
-    Positives=$innertext[$i].innertext
-    City=$innertext[$i+1].innertext
-   
-   }
-$thisobject
-}
-}#end of function
-
-
-write-host "start"
-$positives=positives
-write-host "end"
-
-$object=$ellen|convertFrom-String -TemplateContent $template
-
-$html.all.tags("P")|Select-Object innertext|ConvertFrom-String -TemplateContent $template
-
-
-$html.all.tags("A")| foreach-object innertext
-$html.all.tags("A")| out-file "covid.txt"
-$html.all|out-file "covid.txt"
-$html.all.tags("A")| select-object *
-$html.all.tags("external-html")| where-object innertext -like "*new york city*"
-
-$a=($html.allElements|Where-Object {$_.class -match "feature-list"}).innerText
-$a=($html.allElements|Where-Object {$_.class -match "external-html"}).innerText
-
-$tags=$html.all.tags("")
-
-$url="https://coronavirus.jhu.edu/map.html"
-$response = Invoke-WebRequest -Uri $url
-$response.ParsedHtml.body.getelementsbyclassname('external-html')
-

2e97c4d11e0c464265cb7cc767037a9d8cce6faa
Fred Swims <fredswims@gmail.com>
2020-08-08 00:46:59 -0400

    2020-08-08

--- MyStuff/aaaa.ps1
+++ MyStuff/aaaa.ps1
@@ -31,3 +31,26 @@ param(
 "The value of `$name is {0}." -f $name
 write-host "The value of `$Namer is $($namer)."
 exit $code
+
+
+$ArrayList = New-Object System.Collections.ArrayList
+
+function fShowSubDirectorySize {
+    [Alias('ShowSubDirectorySize')] param ([string[]]$Path = "*")
+    Write-Warning "In function $($MyInvocation.MyCommand.Name): "
+    write-warning "$($path)"
+    $colItems = get-childitem -Directory  -Path $path
+    foreach ($i in $colItems) {
+        $subFolderItems = Get-ChildItem $i.FullName -recurse -force | Where-Object { $_.PSIsContainer -eq $false } | Measure-Object -property Length -sum | Select-Object Sum
+        $thisobject = [pscustomobject]@{
+            Size = $subFolderItems.Sum
+            Name = $i.FullName
+        }
+        $thisobject
+    }
+}
+function fShowFormattedSubDirectorySize{[Alias('ShowFormattedSubDirectorySize')] param ([string[]]$Path="*")
+fShowSubDirectorySize -path $path | sort-object -property size -Descending|Select-Object @{name="Bytes";exp={"{0,16:n0}" -f $psitem.size}}, name
+}
+
+

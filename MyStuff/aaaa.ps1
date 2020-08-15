62d7a2215430430f6c21d4247b70cda1bbd8f0a3
Fred Jacobowitz <fredswims@gmail.com>
2020-08-14 20:49:16 -0400

    2020-08-14

--- MyStuff/aaaa.ps1
+++ MyStuff/aaaa.ps1
@@ -8,7 +8,7 @@ Function GetLastCommand($Number) {
     Write-Host -NoNewline "Last "; Write-Host -NoNewLine -ForegroundColor Green "$($Number) "; Write-Host "commands have been copied to the clipboard."
 }
 
-write-host "lets begin"
+<# write-host "lets begin"
 GetLastCommand -Number 5
 gci
 gci | test-path
@@ -34,7 +34,7 @@ exit $code
 
 
 $ArrayList = New-Object System.Collections.ArrayList
-
+ #>
 function fShowSubDirectorySize {
     [Alias('ShowSubDirectorySize')] param ([string[]]$Path = "*")
     Write-Warning "In function $($MyInvocation.MyCommand.Name): "
@@ -53,4 +53,113 @@ function fShowFormattedSubDirectorySize{[Alias('ShowFormattedSubDirectorySize')]
 fShowSubDirectorySize -path $path | sort-object -property size -Descending|Select-Object @{name="Bytes";exp={"{0,16:n0}" -f $psitem.size}}, name
 }
 
+# Jeffrey Hicks
+Function Convert-TextToNumber {
+    [cmdletbinding()]
+    [outputtype("int32")]
 
+    Param(
+        [Parameter(Position = 0, Mandatory, ValueFromPipeline)]
+        [ValidatePattern("^[A-Za-z]{1,5}$")]
+        [string]$Plaintext,
+        [Parameter(HelpMessage = "A custom dictionary file which is updated everytime a word is converted")]
+        [string]$Dictionary = (join-path $env:TEMP "mywords.txt")
+    )
+
+    Begin {
+        Write-Verbose "[BEGIN  ] Starting: $($MyInvocation.Mycommand)"
+
+        #define a simple hashtable
+        $phone = @{
+            2 = 'abc'
+            3 = 'def'
+            4 = 'ghi'
+            5 = 'jkl'
+            6 = 'mno'
+            7 = 'pqrs'
+            8 = 'tuv'
+            9 = 'wxyz'
+        }
+
+        #initialize a dictionary list
+        # items in a dictionary list may repeat without warning.
+        $dict = [System.Collections.Generic.List[string]]::new()
+        if (Test-Path -path $Dictionary) {
+            Write-Verbose "[BEGIN  ] Loading user dictionary from $Dictionary"
+            (Get-Content -path $Dictionary).foreach({$dict.add($_)})
+            $originalCount = $dict.Count
+            Write-Verbose "[BEGIN  ] Loaded $originalCount items"
+        }
+    } #begin
+    Process {
+        Write-Verbose "[PROCESS] Converting: $Plaintext"
+        #add plaintext to dictionary if missing
+        if ($dict -notcontains $Plaintext) {
+            Write-Verbose "[PROCESS] Adding to user dictionary"
+            $dict.Add($plaintext)
+        }
+
+        #this is a technically a one-line expression
+        #get the matching key for each value and join together
+        #each single digit is pushed to pipeline and returned as a collection.
+        ($plaintext.ToCharArray()).ForEach({
+         $val = $_
+         ($phone.GetEnumerator().Where({$_.value -match $val}).name)}) -join ""
+    } #process
+
+    End {
+        #commit dictionary to file if new words were added
+        if ($dict.count -gt $originalCount) {
+            Write-Verbose "[END    ] Ending: Saving updated dictionary $Dictionary"
+            $dict | Out-File -FilePath $Dictionary -Encoding ascii -Force
+        }
+        Write-Verbose "[END    ] Ending: $($MyInvocation.Mycommand)"
+    } #end
+}
+convert-TextToNumber -Plaintext "anton"
+"eliza"| Convert-TextToNumber
+$fred=(Get-Content -path $Dictionary).foreach({Convert-TextToNumber $psitem})
+
+
+# Condition to test
+$CarColor = 'Blue'
+ 
+# Hashtable with $true & $false as the keys, and the subsequent results as the values
+@{ $true = 'The car color is blue'; $false = 'The car color is not blue'}
+ 
+# Lookup that tests the condition, outputting the relevant value
+[$CarColor -eq 'Blue']
+$true
+
+#hostname
+
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_enum?view=powershell-7
+enum MediaTypes {
+    unknown
+    music = 10
+    mp3
+    aac
+    ogg = 15
+    oga = 15
+    mogg = 15
+    picture = 20
+    jpg
+    jpeg = 21
+    png
+    video = 40
+    mpg
+    mpeg = 41
+    avi
+    m4v
+}
+
+[mediatypes].GetEnumNames()
+[MediaTypes].GetEnumName(15)
+[MediaTypes].GetEnumNames() | ForEach-Object {
+    "{0,-10} {1}" -f $_,[int]([MediaTypes]::$_)
+  }
+
+  [system.environment]::MachineName #fastest
+  "$Pid - $(hostname.exe)" # hostname is available on windows and unix
+  $env:computername
+  (Get-CIMInstance CIM_ComputerSystem).Name

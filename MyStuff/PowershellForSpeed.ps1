0eacf2d9257fad9e22082a95eb987ec19a3157f3
Fred Jacobowitz <fredswims@gmail.com>
2020-08-24 22:40:24 -0400

    update

--- MyStuff/PowershellForSpeed.ps1
+++ MyStuff/PowershellForSpeed.ps1
@@ -18,7 +18,7 @@ foreach ($Num in 1..10000) {
 	if ($Last) { $Array += $Last * $Num } 
 	$Last = $Num
 }
-
+#Array Array List ArrayList 
 $ArrayList = New-Object System.Collections.ArrayList
 foreach ($Num in 1..10) { 
 	if ($Last) { $ArrayList.Add($Last * $Num) }

f9aba92634108086959db6018d8ebe2b30a4922f
Fred Jacobowitz <fredswims@gmail.com>
2021-01-18 22:37:25 -0500

    enabledisablecamera

--- MyStuff/EnableDisableCamera.ps1
+++ MyStuff/EnableDisableCamera.ps1
@@ -1,8 +1,17 @@
-function EnableDisableIt{
     <#
     How to enable and disable the builtin camera or for that matter any pnp device
     https://www.dynamsoft.com/codepool/powershell-disable-enable-webcam-windows.html
     If spawned like pwsh -file EnableDisableCamera.ps1 it must be run elevated
+    Escape quote the string if it has literal spaces in it.
+    start-process -Verb "Runas" -Filepath "pwsh" -ArgumentList "-noprofile -noexit `
+     -command & {. `"C:\Users\freds_000\OneDrive\PowershellScripts\MyStuff\EnableDisableCamera.ps1`" -type status}"
+
+    start-process -Verb "Runas" -Filepath "pwsh" -ArgumentList "-noprofile -noexit `
+     -command & {. `"$thisRun`" -type status}" 
+
+     start-process -Verb "Runas" -Filepath "pwsh" -ArgumentList "-noprofile -noexit `
+-command & {. `"(join-path -path $env:onedrive\PowershellScripts\MyStuff -childpath EnableDisableCamera.ps1)`" -type status}"
+
     #>
     [CmdletBinding()]
      param (
@@ -15,10 +24,13 @@ function EnableDisableIt{
         write-host ""
         Get-PnpDevice -FriendlyName *cam* -Class camera -Status OK
     }
-    if ($Type -eq "Disable") { disable-pnpdevice -InstanceId (Get-PnpDevice -FriendlyName *cam* -Class camera -Status OK).Instanceid }
+    if ($Type -eq "Disable") {
+        disable-pnpdevice -InstanceId (Get-PnpDevice -FriendlyName *cam* -Class camera -Status OK).Instanceid 
+        Get-PnpDevice -FriendlyName *cam* }
+
     if ($Type -eq "Enable") {
         Get-PnpDevice -FriendlyName *cam* -Class camera -Status Error
         enable-pnpdevice -InstanceId (Get-PnpDevice -FriendlyName *cam* -Class camera -Status error).Instanceid
+        Get-PnpDevice -FriendlyName *cam*
     }
-}
 

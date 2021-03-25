f9d3cc2fa87182a7a0d3c9ba77c991e5598426d8
Fred Jacobowitz <fredswims@gmail.com>
2021-03-24 22:06:25 -0400

    refactor

--- MyStuff/cpuTemp.ps1
+++ MyStuff/cpuTemp.ps1
@@ -1,7 +1,7 @@
 # https://social.technet.microsoft.com/Forums/lync/en-US/badf142e-2c34-4d6b-9362-d411e7f3b3a5/get-the-cpu-temperature-with-powershell?forum=ITCG
 # https://www.remkoweijnen.nl/blog/2014/07/18/get-actual-cpu-clock-speed-powershell/
 param (
-    [int32]$Sleep = 60
+    [int32]$Sleep = 60 #seconds
 )
 "In script {0}" -f $MyInvocation.MyCommand.Name
 # Must be elevated
@@ -11,6 +11,7 @@ else {
     "Process must run elevated."; read-host "Enter to terminate"; return
 }
 
+"Retreiving Temperature"
 function GetClockSpeed() {
     $freq = Get-Counter -Counter "\Processor Information(*)\Processor Frequency"
     $freq = Get-Counter -Counter "\Processor Information(*)\Processor Frequency"
@@ -48,36 +49,28 @@ do {
     catch {
         write-warning "$error"        
     }
-    <# 
-    $temps = Get-WMIObject MSAcpi_ThermalZoneTemperature -Namespace "root/wmi"
-    $temps | Select-Object -Property InstanceName,@{n="Temps F";e={(($_.currenttemperature /10 -273.15) *1.8 +32)}}
-     #>
+    <#      
+    $objWMi = get-wmiobject -namespace root\cimv2 -computername localhost -Query "Select * from Win32_PerfFormattedData_Counters_ThermalZoneInformation WHERE Name like '%GFXZ%'"
+    $objWMi = Get-WmiObject MSAcpi_ThermalZoneTemperature -Namespace "root/wmi"
+    foreach ($obj in $objWmi) {
+        write-host "######## INICIO Win32_PerfFormattedData_Counters_ThermalZoneInformation"
+        write-host "Caption:" $obj.Caption
+        write-host "Description:" $obj.Description
+        write-host "Frequency_Object:" $obj.Frequency_Object
+        write-host "Frequency_PerfTime:" $obj.Frequency_PerfTime
+        write-host "Frequency_Sys100NS:" $obj.Frequency_Sys100NS
+        write-host "Name:" $obj.Name
+        write-host "PercentPassiveLimit:" $obj.PercentPassiveLimit
+        write-host "Temperature:" (($obj.Temperature -273.15))
+        write-host "ThrottleReasons:" $obj.ThrottleReasons
+        write-host "Timestamp_Object:" $obj.Timestamp_Object
+        write-host "Timestamp_PerfTime:" $obj.Timestamp_PerfTime
+        write-host "Timestamp_Sys100NS:" $obj.Timestamp_Sys100NS
+        write-host
+        write-host "######## INICIO Win32_PerfFormattedData_Counters_ThermalZoneInformation"
+        write-host
+    }
+ #>
     write-host "Next sample in $($sleep) seconds."
     Start-Sleep -seconds $Sleep
 } while ($true)
-
-<#
-$objWMi = get-wmiobject -namespace root\cimv2 -computername localhost -Query "Select * from Win32_PerfFormattedData_Counters_ThermalZoneInformation WHERE Name like '%GFXZ%'"
-$objWMi = Get-WmiObject MSAcpi_ThermalZoneTemperature -Namespace "root/wmi"
-foreach ($obj in $objWmi)
-{
-write-host "######## INICIO Win32_PerfFormattedData_Counters_ThermalZoneInformation"
-write-host "Caption:" $obj.Caption
-write-host "Description:" $obj.Description
-write-host "Frequency_Object:" $obj.Frequency_Object
-write-host "Frequency_PerfTime:" $obj.Frequency_PerfTime
-write-host "Frequency_Sys100NS:" $obj.Frequency_Sys100NS
-write-host "Name:" $obj.Name
-write-host "PercentPassiveLimit:" $obj.PercentPassiveLimit
-write-host "Temperature:" (($obj.Temperature -273.15))
-write-host "ThrottleReasons:" $obj.ThrottleReasons
-write-host "Timestamp_Object:" $obj.Timestamp_Object
-write-host "Timestamp_PerfTime:" $obj.Timestamp_PerfTime
-write-host "Timestamp_Sys100NS:" $obj.Timestamp_Sys100NS
-write-host
-write-host "######## INICIO Win32_PerfFormattedData_Counters_ThermalZoneInformation"
-write-host
-
-}
-
-#>

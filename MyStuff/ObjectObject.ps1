a319717b5be78d373c3a92f11d8315c5d96ce5c3
Fred Jacobowitz <fredswims@gmail.com>
2021-06-12 19:39:08 -0400

    ObjectObject

--- MyStuff/ObjectObject.ps1
+++ MyStuff/ObjectObject.ps1
@@ -1,5 +1,16 @@
 
 #ObjectObject.ps1
+
+
+# some command | select-object x,y z |
+[PSCustomObject]@{
+    Date      = $_.TimeGenerated
+    Name      = $_.ReplacementStrings[0]
+    Path      = $_.ReplacementStrings[1]
+    StartMode = $_.ReplacementStrings[3]
+    User      = $_.ReplacementStrings[4]
+}
+
 function makeanobject {
 
  $thisobject=   [pscustomobject]@{Name='Test';Type='Type002';AnotherKey="some value"} #create the object

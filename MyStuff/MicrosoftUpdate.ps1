8b99ef43cfa7734d9b0426330491c960ed9fa272
Fred Swims <fredswims@gmail.com>
2021-06-18 19:04:19 -0400

    MicrosoftUpdate

---
+++ MyStuff/MicrosoftUpdate.ps1
@@ -0,0 +1,7 @@
+# https://devblogs.microsoft.com/powershell/preview-updating-powershell-7-2-with-microsoft-update/S
+$pwshRegPath = "HKLM:\SOFTWARE\Microsoft\PowerShellCore"
+if (!(Test-Path -Path $pwshRegPath)) {
+    throw "PowerShell 7 is not installed"
+}
+
+Set-ItemProperty -Path $pwshRegPath -Name UseMU -Value 1 -Type DWord

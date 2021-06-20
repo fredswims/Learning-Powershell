7974aefda6fe52f5e054d8cc1b3a39d236017c0b
Fred Swims <fredswims@gmail.com>
2021-06-19 20:35:13 -0400

    2021-06-19

--- MyStuff/MicrosoftUpdate.ps1
+++ MyStuff/MicrosoftUpdate.ps1
@@ -1,4 +1,5 @@
 # https://devblogs.microsoft.com/powershell/preview-updating-powershell-7-2-with-microsoft-update/S
+# must be elevated
 $pwshRegPath = "HKLM:\SOFTWARE\Microsoft\PowerShellCore"
 if (!(Test-Path -Path $pwshRegPath)) {
     throw "PowerShell 7 is not installed"

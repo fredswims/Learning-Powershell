b92fdfe92b249597d27c876ff3b6d0c0671e5d4d
Fred Jacobowitz <fredswims@gmail.com>
2022-09-23 13:58:12 -0400

    version 5

--- MyStuff/FrednEllen.ps1
+++ MyStuff/FrednEllen.ps1
@@ -1,6 +1,10 @@
-begin version 1
-this is the first @'
-this is he second line
-this is the third line
-'@
-END version one
+# begin version 3
+# this is the first @'
+# this is he second line
+# added this line version 2
+# version 4
+# this is the third line
+# fred 
+# END version one
+# verson 3
+# verssoni 5

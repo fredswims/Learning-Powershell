e9485cc3655c46df2f80c1139b1197fe50b736b3
Fred Jacobowitz <fredswims@gmail.com>
2023-03-27 20:33:16 -0400

    RunFanContinuously

--- MyStuff/GetClockSpeed.ps1
+++ MyStuff/GetClockSpeed.ps1
@@ -4,5 +4,7 @@ $CurrentClockSpeed = [math]::round(($MaxClockSpeed/1000) * ($ProcessorPerformanc
 
 Write-Host "Current Processor Speed in GHz: " -ForegroundColor Yellow -NoNewLine
 Write-Host $CurrentClockSpeed -NoNewline
+Write-Host "  Performance " -ForegroundColor Yellow -NoNewLine
+Write-Host " $([math]::round(($ProcessorPerformance / 100),3))" -NoNewline
 Write-Host "  Max Clock Speed in GHz: " -ForegroundColor Yellow -NoNewLine
 Write-Host ($MaxClockSpeed / 1000)

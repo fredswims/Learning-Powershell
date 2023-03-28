ff2d67451417844abba1c41f47d1d3e24105195b
Fred Jacobowitz <fredswims@gmail.com>
2023-03-27 20:22:35 -0400

    changes to minimized

---
+++ MyStuff/GetClockSpeed.ps1
@@ -0,0 +1,8 @@
+$MaxClockSpeed = (Get-CimInstance CIM_Processor).MaxClockSpeed
+$ProcessorPerformance = (Get-Counter -Counter "\Processor Information(_Total)\% Processor Performance").CounterSamples.CookedValue
+$CurrentClockSpeed = [math]::round(($MaxClockSpeed/1000) * ($ProcessorPerformance / 100),3)
+
+Write-Host "Current Processor Speed in GHz: " -ForegroundColor Yellow -NoNewLine
+Write-Host $CurrentClockSpeed -NoNewline
+Write-Host "  Max Clock Speed in GHz: " -ForegroundColor Yellow -NoNewLine
+Write-Host ($MaxClockSpeed / 1000)

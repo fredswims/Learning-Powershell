074d662cea8d4c8bc5bf22a79363d3c80ee2f098
Fred Jacobowitz <fredswims@gmail.com>
2023-03-28 15:26:32 -0400

    changes

--- MyStuff/RenameFiles.ps1
+++ MyStuff/RenameFiles.ps1
@@ -1,7 +1,7 @@
 ﻿set-location $env:TEMP
 $path="config.log"
 remove-item -force -ErrorAction SilentlyContinue -path $path
-new-item -ItemType file -path $path 
+new-item -ItemType file -path $path
 # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_script_blocks?view=powershell-7
 # Using *** delay-bind script blocks *** with parameters
 # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_script_blocks?view=powershell-7#using-delay-bind-script-blocks-with-parameters
@@ -43,49 +43,56 @@ $file="fred.txt"
 new-item -type file -Path $file
 $splat=@{
   Path=$file
-  NewName="fred" 
+  NewName="fred"
   }
 Rename-item @splat
-  
-Rename-Item -path fred.txt 
+
+Rename-Item -path fred.txt
 
 rename-item -Path $fred.name -NewName ($datesubstring + $fredroot + $fred.Extension)
 rename-item -Path $fred.name -NewName ($fred.BaseName.Substring($fred.BaseName.IndexOf("20"),$fred.BaseName.Length) + $fred.BaseName.Substring(0,$fred.BaseName.IndexOf("20")) + $fred.Extension)
 
 $fred.BaseName.IndexOf("20"),$fred.BaseName.Length
- $datesubstring=$fred.BaseName.Substring($fred.BaseName.IndexOf("20"),$fred.BaseName.Length ) 
+ $datesubstring=$fred.BaseName.Substring($fred.BaseName.IndexOf("20"),$fred.BaseName.Length )
  $datestart=$fred.BaseName.IndexOf("20")
- $datelength=$fred.BaseName.Length  
-  $fred.BaseName.Substring(0,$datestart) 
+ $datelength=$fred.BaseName.Length
+  $fred.BaseName.Substring(0,$datestart)
 
-rename-item -path $fred.name -NewName () 
-18 get-item fred*    
-
-                                                                               
-  19 $fred=get-item fred*                                                                             
-  20 $fred.BaseName                                                                                   
-  21 $fred.BaseName.Substring(1,5)                                                                    
-  22 $fred.BaseName.Substring(1,$fred.BaseName.Length)                                                
-  23 $fred.BaseName.IndexOf("20")                                                                     
-  24 $datestart=$fred.BaseName.IndexOf("20")                                                          
-  25 $datestart                                                                                       
-  26 $datelength=$fred.BaseName.Length                                                                
-  27 $datelength                                                                                      
-  28 $datelength=$fred.BaseName.Length                                                                
-  29 $datelength=$fred.BaseName.Length - $datestart                                                   
-  30 $datelength                                                                                      
-  31 $fred.BaseName.Substring($datestart,$datelength)                                                 
-  32 $datesubstring=$fred.BaseName.Substring($datestart,$datelength)                                  
-  33 $fred.BaseName.Substring(0,$datestart-1)                                                         
-  34 $fred.BaseName.Substring(0,$datestart)                                                           
-  35 $fredroot=$fred.BaseName.Substring(0,$datestart)                                                 
-  36 $datesubstring                                                                                   
-  37 $datesubstring + $fredroot                                                                       
-  38 $datesubstring + $fredroot + $fred.Extension                                                     
-  39 rename-item -Path $fred.name -NewName ($fred.BaseName.Substring($fred.BaseName.IndexOf("20") ,($fred.BaseName.Length - $fred.BaseName.IndexOf("20"))) + $fred.BaseName.Substring(0,($fred.BaseName.IndexOf("20")) + $fred.Extension)             
-  40 ls *fre*  
+rename-item -path $fred.name -NewName ()
+18 get-item fred*
 
 
-  
+  19 $fred=get-item fred*
+  20 $fred.BaseName
+  21 $fred.BaseName.Substring(1,5)
+  22 $fred.BaseName.Substring(1,$fred.BaseName.Length)
+  23 $fred.BaseName.IndexOf("20")
+  24 $datestart=$fred.BaseName.IndexOf("20")
+  25 $datestart
+  26 $datelength=$fred.BaseName.Length
+  27 $datelength
+  28 $datelength=$fred.BaseName.Length
+  29 $datelength=$fred.BaseName.Length - $datestart
+  30 $datelength
+  31 $fred.BaseName.Substring($datestart,$datelength)
+  32 $datesubstring=$fred.BaseName.Substring($datestart,$datelength)
+  33 $fred.BaseName.Substring(0,$datestart-1)
+  34 $fred.BaseName.Substring(0,$datestart)
+  35 $fredroot=$fred.BaseName.Substring(0,$datestart)
+  36 $datesubstring
+  37 $datesubstring + $fredroot
+  38 $datesubstring + $fredroot + $fred.Extension
+  39 rename-item -Path $fred.name -NewName ($fred.BaseName.Substring($fred.BaseName.IndexOf("20") ,($fred.BaseName.Length - $fred.BaseName.IndexOf("20"))) + $fred.BaseName.Substring(0,($fred.BaseName.IndexOf("20")) + $fred.Extension)
+  40 ls *fre*
+
+
+
   new-item $file
   rename-item -confirm -path $file  -NewName "fred$($fred.basename.replace('f','').replace('ed','yellow'))Ellen$($fred.Extension)"
+
+
+
+  # Place "." after YYYY-MM-DD
+  get-item 20* | rename-item -confirm -NewName {$_.name.insert($_.name.lastindexof("-")+3,".")}
+  # Remove spaces
+  get-item 2011* | rename-item -confirm -NewName {$_.name.replace(" ","")}

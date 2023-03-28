70464e35bc25f51aabcd0ffd7d44df1b8a327456
Fred Jacobowitz <fredswims@gmail.com>
2023-03-28 13:50:40 -0400

    compact message and Window Title inclusive

---
+++ MyStuff/runFanContinuously.ps1
@@ -0,0 +1,36 @@
+# https://slai.github.io/posts/powershell-and-external-commands-done-right/
+[CmdletBinding()]
+param (
+    [Parameter(ValueFromPipeline = $true)]
+    [Int32]$Delay=360,
+    [Parameter(Mandatory = $false)]
+    [String]$Once = 'N',
+    [Parameter(Mandatory = $false)]
+    [switch]$AsFSInfo = $false
+)
+Write-Warning "In module $($MyInvocation.MyCommand.Name): "
+$host.UI.RawUI.WindowTitle = "RunFan Delay {0} TaskId {1}" -f $Delay, $pid
+$exec = (join-path -path ${env:ProgramFiles(x86)} -ChildPath "Sony\VAIO System Diagnostic\CPU Fan Diagnostic\FDU.exe")
+if ($Once -like 'Y') { 
+    "In run 'Once' mode"    
+    &$exec # invoke fan program using call operator and return immediately.
+}
+else {
+    "Running continuously - kill process {0}" -f $pid
+    for ($i = 1; $true; $i++) {
+        # write-warning $delay
+        # write-warning "Count $($i)"
+        "Running Fan - Count {0}{1}{2}" -f $PSStyle.Formatting.Warning,$i,$psstyle.reset
+        # write-warning $pid
+        # write-warning ' Running Fan'
+        # &$exec
+        start-process -WindowStyle Minimized (resolve-path (join-path -path ${env:ProgramFiles(x86)} -ChildPath "Sony\VAIO System Diagnostic\CPU Fan Diagnostic\FDU.exe")).path
+        # start /min "C:\Program Files (x86)\Desktop Secretary"
+        # start /min "C:\Program Files (x86)\Desktop Secretary"
+        # cmd /c start /MIN 'C:\Program Files (x86)\Sony\VAIO System Diagnostic\CPU Fan Diagnostic\FDU.exe'
+        & C:\Users\freds_000\OneDrive\PowershellScripts\MyStuff\GetClockSpeed.ps1
+        # ./getclockspeed.ps1
+        Get-Date -DisplayHint Time
+        Start-Sleep -seconds $delay
+    }
+}

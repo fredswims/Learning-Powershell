e12724e36d73347e7d364f76c9491ed50b276821
Fred Jacobowitz <fredswims@gmail.com>
2023-04-07 01:03:20 -0400

    OpenIA Stuff

---
+++ MyStuff/openAIstuff.ps1
@@ -0,0 +1,8 @@
+# https://github.com/dfinke/PowerShellAI 
+
+# https://github.com/dfinke/PowerShellAI/wiki/PowerShellChatGPT 
+# https://www.youtube.com/watch?v=ZX5gwzJe7eI 
+install-PSResource -Name PowerShellAI -Reinstall -Verbose
+$env:OpenAIKey='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'
+ai "what does a high than normal METHYLMALONIC ACID  and normal vitamin b-12 level indicate?"
+invoke-item (get-dalleimage "a picture of a cat")

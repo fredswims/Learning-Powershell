016a607a4c24ddeb550e29de1dd5e1efc559fb01
Fred Jacobowitz <fredswims@gmail.com>
2023-06-08 22:48:40 -0400

    2023-06-8 - update window title with clock speed

--- MyStuff/runFanContinuously.ps1
+++ MyStuff/runFanContinuously.ps1
@@ -1,4 +1,4 @@
-# 2023-06-5.1
+# 2023-06-8
 # https://slai.github.io/posts/powershell-and-external-commands-done-right/
 [CmdletBinding()]
 param (
@@ -12,14 +12,17 @@ $host.UI.RawUI.WindowTitle = "RunFan Delay {0} Id {1}" -f $Delay, $pid
 $exec = (join-path -path ${env:ProgramFiles(x86)} -ChildPath "Sony\VAIO System Diagnostic\CPU Fan Diagnostic\FDU.exe")
 if ($Once.IsPresent) { 
     "In run 'Once' mode"    
-    &$exec # invoke fan program using call operator and return immediately.
+    & $exec # invoke fan program using call operator and return immediately.
 }
 else {
     "Run interval on {1} seconds. - Kill process {0}" -f $pid, $Delay
     for ($i = 1; $true; $i++) {
         # write-warning $delay
         # write-warning "Count $($i)"
-        & C:\Users\freds_000\OneDrive\PowershellScripts\MyStuff\GetClockSpeed.ps1
+        # $Speed=& (Join-path $env:OneDrive "PowershellScripts\MyStuff\GetClockSpeed.ps1")
+        $Speed=invoke-expression (Join-path $env:OneDrive "PowershellScripts\MyStuff\GetClockSpeed.ps1")
+        $host.UI.RawUI.WindowTitle = "RunFan [{2} GHz] Delay {0} Id {1}" -f $Delay, $pid, $speed
+        # if ($speed[0] -eq "-"){New-BurntToastNotification -text $speed -Silent}
         # $Time=Get-Date -DisplayHint Time
         $Time=get-date -UFormat '%I:%M:%S %p'
         "<{3}> - Running Fan - Count {0}{1}{2}" -f $PSStyle.Formatting.Warning,$i,$psstyle.reset,$Time

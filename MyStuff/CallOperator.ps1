47be476d458bfc0adf41363b619f0041ba8fcbeb
fredswims <fredswims@gmail.com>
2024-11-01 18:40:49 -0400

    Call Operator

---
+++ MyStuff/CallOperator.ps1
@@ -0,0 +1,11 @@
+/some things you can do with the call operator
+$command = "Get-ChildItem"
+& $command
+
+  & "notepad.exe"
+  $scriptBlock = { Get-Process | Where-Object { $_.Name -eq "notepad" } }
+  & $scriptBlock
+
+  start-process powershell -Args " -noprofile -command & {. '$runThis' $arg -speak }"
+
+#   https://ss64.com/ps/call.html

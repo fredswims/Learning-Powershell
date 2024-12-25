e9513fdfe3bc9fff6861a97ca459ff5fc2b39665
fredswims <fredswims@gmail.com>
2024-12-24 21:16:58 -0500

    Add New-object_PsObject.ps1 script to process function lines

---
+++ MyStuff/Object.New-object_PsObject.ps1
@@ -0,0 +1,15 @@
+$results = @()
+$Number=0 #initialize the counter
+# $file = 'C:\protected_temp\logs\intest.log'
+$pattern = 'function.*fjs'
+Sprofile $pattern  | ForEach-Object {
+    $ThisLine=$_.line #the actual line of text returned
+    $ThisLineNumber=$_.LineNumber #the line number of the file
+    $Number+=1
+    $props = @{
+        Line=$ThisLIne
+        LineNumber=$ThisLineNumber
+        Number=$Number
+    }
+    $results += New-Object psObject -Property $props #add this line to the results array which is an array of objects.
+    }

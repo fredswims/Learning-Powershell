eabb09038d3e829d50fe7530e6c2cc5910d8deac
fredswims <fredswims@gmail.com>
2025-01-07 20:31:16 -0500

    PSModulePath

---
+++ MyStuff/PSModulePath.ps1
@@ -0,0 +1,18 @@
+<# 
+$dirs=($env:PSModulePath).Split(";")
+foreach ($dir in $dirs) {
+    (test-path $dir) ? ($(set-location $dir;read-host "[$(get-location)]";$(get-item *).Name ;read-host "[pause]")) : "false"}
+
+ #>
+ Clear-Host
+$dirs = ($env:PSModulePath).Split(";")
+foreach ($dir in $dirs) {
+    if (test-path $dir) {
+        # set-location $dir
+        read-host "[$dir]"
+        $(get-item -path (Join-Path $dir "*")).Name
+        read-host "[Next]"
+    }
+    else { read-host "[$dir] Folder does not exist" }
+}
+    

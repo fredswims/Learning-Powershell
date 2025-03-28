59e1291d5f83addc51b4e7f83de669d093e03c7e
fredswims <fredswims@gmail.com>
2025-03-28 02:32:01 -0400

    FAJ 2025-03-28

--- MyStuff/get-parent.ps1
+++ MyStuff/get-parent.ps1
@@ -5,14 +5,16 @@ param (
     [Int64]$Id,
     [switch]$NoExit=$false
 )
+    # FAJ 2025-03-28
     $thisErrorCount=$Error.Count
     $thisErrorActionPreference=$ErrorActionPreference
     $ErrorActionPreference='silentlycontinue'
     $parent=$null
     $Parent=get-process -ErrorAction SilentlyContinue -id (get-process -ErrorAction SilentlyContinue  -id $id).parent.id
     if ($Parent.id -gt 0) {
-        'Parent ID: {0}' -f $parent.id
-        '      Parent Name: {0}' -f $parent.name
+        # 'Parent ID: {0}' -f $parent.id
+        '[{1}] [{2:d6}] [{0}] [{3}]' -f $parent.name , $parent.StartTime, $parent.id, $parent.path
+        # $parent|Format-Table -HideTableHeaders -AutoSize -Property  id,  starttime, name, path
         $ErrorActionPreference = $thisErrorActionPreference
         if ($thisErrorCount -gt $error.count) {
             $error.Clear()
@@ -25,13 +27,19 @@ param (
             $error.Clear()
         }
         $ErrorActionPreference = $thisErrorActionPreference
-        write-host "Finished"
+        write-host "Finished Get-Parent.ps1"
         if ($noexit){Read-Host "Paused. Press Enter to exit."}
     }
     # $error.Clear()
-}
+} # end Function Get-Parent
+
 # Clear-Host
-'starting'
-'       ID: {0}' -f $pid
-'             Name: {0}' -f (Get-process -id $pid).name
+Write-Warning "In Script $($MyInvocation.MyCommand.Name): "
+
+'This Process and Parents'
+# '       ID: {0}' -f $pid
+# '             Name: {0}' -f (Get-process -id $pid).name
+$ThisProcess=get-process -id $pid # get the current process
+'[{1}] [{2:d6}] [{0}] [{3}]' -f $ThisProcess.name , $ThisProcess.StartTime, $ThisProcess.id, $ThisProcess.path
+
 getparent $PID

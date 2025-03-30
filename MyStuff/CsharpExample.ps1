0aa33e9aa296d0ecceb194471f0e710c076834d1
fredswims <fredswims@gmail.com>
2025-03-30 00:33:09 -0400

    FAJ 2025-03-30

---
+++ MyStuff/CsharpExample.ps1
@@ -0,0 +1,32 @@
+# https://copilot.microsoft.com/shares/YnzhYBiweiBub8w48fx9W
+# https://copilot.microsoft.com/shares/cj99LpyzeV4LjE8pZh9fK
+
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [Int64]$Count=10
+)
+$source = @"
+using System;
+using System.Threading.Tasks;
+
+public class ParallelExample
+{
+    public static void Run()
+    {
+        Parallel.For(0, 100, i =>
+        {
+            Console.WriteLine($"Task {i} is running on thread {System.Threading.Thread.CurrentThread.ManagedThreadId}");
+        });
+
+        Console.WriteLine("All tasks completed.");
+    }
+}
+"@
+
+Add-Type -TypeDefinition $source -Language CSharp
+for ($i=0; $i -lt $Count; $i++) {[ParallelExample]::Run()}
+# [ParallelExample]::Run()
+
+
+# https://copilot.microsoft.com/shares/71MtihfHpcM7judE9wj9X

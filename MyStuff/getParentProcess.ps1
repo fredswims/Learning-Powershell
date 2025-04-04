359af0c6cc1417d49215664047f435685cc15ae5
fredswims <fredswims@gmail.com>
2025-04-04 00:32:35 -0400

    FAJ 2025-04-04 Happy Birthday

--- MyStuff/getParentProcess.ps1
+++ MyStuff/getParentProcess.ps1
@@ -1,4 +1,12 @@
 # FAJ 2025-03-28
+# List the Calling Process and its Parents.
+<# 
+Function PrintLine {
+    param($Process)
+    "[{2,8}] {4}[{5}{0,-15}{4}]{5} [{1:yyyy-MM-dd hh:mm:ss tt}] [{3}]" `
+        -f $Process.name , $Process.StartTime, $Process.id, $Process.path, $psStyle.Formatting.Warning, $psstyle.reset
+}
+ #>
 [CmdletBinding()]
 param (
     # [Parameter()]
@@ -11,19 +19,21 @@ param (
 )
 Begin {
     # [int64]$parentId = 0
-    if ((get-process).id -contains $id) {"Good Id"} else {"Enter a valid 'Id'";return}
+    if ((get-process).id -contains $id) { "Good Id" } else { Write-host -ForegroundColor Red  "Enter a valid 'Id'"; return }
     Write-Warning "In Script $($MyInvocation.MyCommand.Name): "
     write-host -ForegroundColor Yellow 'This Process and Parents'
     # get the current process
     $ThisProcess = get-process -id $Id
-    '{4}[{5}{0}{4}]{5} [{2:d6}] [{1}] [{3}]' -f $ThisProcess.name , $ThisProcess.StartTime, $ThisProcess.id, $ThisProcess.path, $psStyle.bold, $psstyle.boldoff
+    "[{2,8}] {4}[{5}{0,-15}{4}]{5} [{1:yyyy-MM-dd hh:mm:ss tt}] [{3}]" `
+        -f $ThisProcess.name , $ThisProcess.StartTime, $ThisProcess.id, $ThisProcess.path, $psStyle.Formatting.Warning, $psstyle.reset
     $ParentId = $(get-process -ErrorAction SilentlyContinue -id $id).parent.id
 
 }
 Process {
     while ($ParentId -gt 0) {
         $ParentProcess = get-process -id $ParentId -ErrorAction SilentlyContinue
-        '{4}[{5}{0}{4}]{5} [{2:d6}] [{1}] [{3}]' -f $ParentProcess.name , $ParentProcess.StartTime, $ParentProcess.id, $ParentProcess.path, $psStyle.bold, $psstyle.boldoff
+        "[{2,8}] {4}[{5}{0,-15}{4}]{5} [{1:yyyy-MM-dd hh:mm:ss tt}] [{3}]" `
+            -f $ParentProcess.name , $ParentProcess.StartTime, $ParentProcess.id, $ParentProcess.path, $psStyle.Formatting.Warning, $psstyle.reset
         $Id = $ParentProcess.id
         $ParentId = $(get-process -ErrorAction SilentlyContinue -id $Id).parent.id
     }
@@ -32,7 +42,3 @@ End {
         write-host "Finished GetParentProcess.ps1" -ForegroundColor yellow
         if ($noexit) { Read-Host "Paused. Press Enter to exit." }
 } 
-<# 
-$tasks=get-process
-if ($tasks.id -contains $pid) {"true"}else {"false"}
- #>

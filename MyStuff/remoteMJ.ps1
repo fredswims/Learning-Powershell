293fb237ca5614b83cc8d56117cdf9a5b061a163
fredswims <fredswims@gmail.com>
2025-06-22 00:36:36 -0400

    RemoteToMJ

--- MyStuff/remoteMJ.ps1
+++ MyStuff/remoteMJ.ps1
@@ -5,8 +5,10 @@ Author: Fred Jacobowitz
 Date: 2025-06-16
  #>
 Clear-Host 
+    # Write-Warning "In Script $($MyInvocation.MyCommand.Name): "
+    write-host "Beginning script $PSCommandPath on $env:computername" 
 write-host "setting up variables"
-$remoteSystem="mj"
+$remoteSystem = "mj"
 $remoteFolder = (join-path "C:\Users\Maraj\Onedrive"-ChildPath Documents)
 $remoteFile = "MarasServices.txt"
 $remoteFullPath = (join-path -path $remoteFolder -ChildPath $remoteFile)
@@ -22,40 +24,55 @@ $thisComputerFredsFileFullPath = (join-path -Path $thisComputerFolder -ChildPath
 Get-PSSession | Remove-PSSession
 # Measure-Command -Expression {$session = New-PSSession -ComputerName mj}
 # Can the remote system can accept PowerShell remoting connections?
-
-if (Test-WsMan -ComputerName $remoteSystem) {
-    Write-Host "The remote system $remoteSystem is Online" 
-} 
-else { 
-    Write-Host "The remote system $remoteSystem is OffLine" 
-    throw "The remote system $remoteSystem is OffLine"
+try {
+    
+    Write-Host "Is \\$remoteSystem Online?"
+    if (Test-WsMan -ComputerName $remoteSystem) {
+        Write-Host "The remote system \\$remoteSystem is Online" 
+    } 
+    else { 
+        Write-Warning "The remote system $remoteSystem is OffLine" 
+        throw "The remote system $remoteSystem is OffLine"
+    }
+    
+    
+    Write-Host "Get-PSSession"
+    $session = New-PSSession -ComputerName $remoteSystem
+    
+    Invoke-Command -session $session -scriptblock {get-process -id $pid} 
+    Invoke-Command -session $session -scriptblock { 
+        if (Test-Path $using:remoteFullPath ) { remove-item $using:remoteFullPath -Verbose }
+    }
+    Invoke-Command -session $session -scriptblock {
+        $ServicesRemote = Get-Service -Name * 
+        # the next line can be executed locally. out-file $remoteFile
+        $ServicesRemote | sort-object name | out-file $using:remoteFullPath -Verbose
+    }
+    Invoke-Command -session $session -scriptblock {
+        get-item $using:remoteFullPath | Select-Object fullname, LastWriteTime
+    }
+    Copy-Item -Verbose -FromSession $session -Path $remotefullpath `
+        -Destination (Join-path $thisComputerFolder -ChildPath $remoteFile)
+    
+    if (Test-Path $thisComputerFredsFileFullPath ) { remove-item $thisComputerFredsFileFullPath -Verbose }    
+    $Services = Get-Service -Name * 
+    $Services | sort-object name | out-file -FilePath $thisComputerFredsFileFullPath -verbose
+    Get-ChildItem -path @($thisComputerFredsFileFullPath, $thisComputerMarasFileFullPath) | `
+        format-list -Property FullName, LastWriteTime, Length
 }
 
- 
-Write-Host "Get-PSSession"
-$session = New-PSSession -ComputerName $remoteSystem
+catch {
+    <#Do this if a terminating exception happens#>
+    write-host "In the catch block."
+}
 
-Invoke-Command -session $session -scriptblock { 
-    if (Test-Path $using:remoteFullPath ) {remove-item $using:remoteFullPath -Verbose}
+finally {
+    <#Do this after the try block regardless of whether an exception occurred or not#>
+    write-host "In the Finally block"
+    write-host "remove sessions"
+    (Get-PSSession | Remove-PSSession)
+    Write-Host  "$pscommandpath Finis"
 }
-Invoke-Command -session $session -scriptblock {
-    $ServicesRemote=Get-Service -Name * 
-    # the next line can be executed locally. out-file $remoteFile
-    $ServicesRemote | sort-object name | out-file $using:remoteFullPath -Verbose
-}
-Invoke-Command -session $session -scriptblock {
-    get-item $using:remoteFullPath | Select-Object fullname, LastWriteTime
-}
-Copy-Item -Verbose -FromSession $session -Path $remotefullpath `
-    -Destination (Join-path $thisComputerFolder -ChildPath $remoteFile)
-
-if (Test-Path $thisComputerFredsFileFullPath ) {remove-item $thisComputerFredsFileFullPath -Verbose}    
-$Services=Get-Service -Name * 
-$Services | sort-object name | out-file -FilePath $thisComputerFredsFileFullPath -verbose
-Get-ChildItem -path @($thisComputerFredsFileFullPath, $thisComputerMarasFileFullPath) | `
-    format-list -Property FullName, LastWriteTime, Length
-(Get-PSSession | Remove-PSSession)
-Write-Host "Finis"
 <#
 Compare-Object `
  -ReferenceObject (get-content $thisComputerFredsFileFullPath) `

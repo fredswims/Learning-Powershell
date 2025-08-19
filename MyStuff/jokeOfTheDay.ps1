fe2b528893c83f0eae859667fd3f489cca6fee25
fredswims <fredswims@gmail.com>
2025-08-18 22:48:48 -0400

    JokeOfTheDay

---
+++ MyStuff/jokeOfTheDay.ps1
@@ -0,0 +1,19 @@
+Invoke-RestMethod -Uri "https://icanhazdadjoke.com/" -Headers @{Accept="text/plain"}
+
+$joke = Invoke-RestMethod -Uri "https://icanhazdadjoke.com/" -Headers @{Accept="application/json"}
+$joke.joke
+
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.MessageBox]::Show($joke.joke, "Joke of the Day")
+
+#Install-Module -Name BurntToast
+New-BurntToastNotification -Text "Joke of the Day", $joke.joke
+
+#Place headers in a hash table.
+$headers = @{
+    Accept="application/json"
+    UserAgent="Fred's PowerShell JokeBot"
+}
+$url = "https://icanhazdadjoke.com/"
+$joke=Invoke-RestMethod -Uri $url -Headers $headers
+$joke.joke

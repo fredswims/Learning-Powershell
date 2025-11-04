f96c3664deeef54d22fedccdd7c4156a301c4e98
fredswims <fredswims@gmail.com>
2025-11-03 21:53:14 -0500

    BetterSplatting

--- MyStuff/EnableDisableCamera.ps1
+++ MyStuff/EnableDisableCamera.ps1
@@ -1,17 +1,23 @@
 <#
     How to enable and disable the builtin camera or for that matter any pnp device
     https://www.dynamsoft.com/codepool/powershell-disable-enable-webcam-windows.html
-    If spawned like pwsh -file EnableDisableCamera.ps1 it must be run elevated
-    Escape quote the string if it has literal spaces in it.
-    start-process -Verb "Runas" -Filepath "pwsh" -ArgumentList "-noprofile -noexit `
-     -command & {. `"C:\Users\freds_000\OneDrive\PowershellScripts\MyStuff\EnableDisableCamera.ps1`" -type status}"
+    $thisScript = $(join-path $env:onedrive PowershellScripts\MyStuff EnableDisableCamera.ps1 -Resolve)
+    # splat the arguments to start-process and use 'Argumentlist' parameters as array to avoid quoting issues.
+    $splat = @{
+        FilePath     = "pwsh"
+        Verb         = "RunAs"
+        ArgumentList = @(
+            "-NoProfile",
+            "-NoExit",
+            "-File", $thisScript,
+            "-type", $Action
+        )
+    }
+    $json = $splat | ConvertTo-Json -Depth 3
+    Write-Output $json
 
-    start-process -Verb "Runas" -Filepath "pwsh" -ArgumentList "-noprofile -noexit `
-     -command & {. `"$thisRun`" -type status}" 
-
-     start-process -Verb "Runas" -Filepath "pwsh" -ArgumentList "-noprofile -noexit `
--command & {. `"(join-path -path $env:onedrive\PowershellScripts\MyStuff -childpath EnableDisableCamera.ps1)`" -type status}"
-
+    Start-Process @splat
+    Remove-Variable thisScript
     #>
 [CmdletBinding()]
 param (
@@ -25,7 +31,7 @@ function ShowStatus {
     write-host ""
     #Get-PnpDevice -FriendlyName *cam* -Class camera -Status OK
 }
-Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+write-host ( $(get-date -format yyyy-MM-dd' 'HH:mm:ss) )
 $PSCommandPath
 
 if ($Type -eq "Status") {

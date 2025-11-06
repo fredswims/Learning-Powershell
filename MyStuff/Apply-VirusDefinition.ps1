2ed60d9d4b038f591c88517c7bbfdf4f6582b592
fredswims <fredswims@gmail.com>
2025-11-05 23:34:22 -0500

    refactor

--- MyStuff/Apply-VirusDefinition.ps1
+++ MyStuff/Apply-VirusDefinition.ps1
@@ -1,52 +1,45 @@
-function fjApplyVirusDefinition {
+Function fjApplyVirusDefinition {
+    param ([Int32]$Seconds = 15)
     # FAJ Sept 2024 
     # File -> 'Apply-VirusDefinition.ps1'
     # https://www.microsoft.com/en-us/wdsi/defenderupdates
-    Write-Warning "In function $($MyInvocation.MyCommand.Name):"
-    
-    #diagnostic info
-        "Process [{0}] Parent [{1}]" -f `
-            (get-process -Id $pid).processname, (get-process -Id $pid).Parent.name
-        get-host -verbose
-        "*****PSCommandPath***************" 
-        $PSCommandPath|format-list *
-        "*********************"
-    #end diagnostic info
-    
-    $file = join-path $env:HOME "Downloads\mpam-fe.exe"
+    write-warning "In [$PSCommandPath]"
+
+    $file = (Join-Path $HOME 'Downloads\mpam-fe.exe') # file path to download latest virus definition file.
     $sourceUrl = "https://go.microsoft.com/fwlink/?LinkID=121721&arch=arm64"
-    if (test-path -Verbose -Path $file) { remove-item $file -Verbose }
-    # download latest virus definition file.
-    # Use Start-BitsTransfer to download the file. 
+    if (test-path -Path $file) { remove-item $file -Verbose }
+    
+    # Use Start-BitsTransfer to download the latest virus definition file. 
     $parameters = @{
         Source      = $sourceUrl
         Destination = $file
         DisplayName = 'Threat detections for Microsoft Defender Antivirus and other Microsoft antimalware.'
     }
-    "Show Parameters"
-    $parameters
     Start-BitsTransfer @parameters -verbose
-    # Start-BitsTransfer -Source $sourceUrl -Destination $file -DisplayName $file -Verbose
 
     # apply the file
     if (test-path $file) { 
         # wait for file to be applied - like invoke-command but waits for it to complete.
+        Write-Warning "Applying file '$file'..."
         start-process -Verbose -Wait -filepath $file
         write-warning "$file applied"
+
+        # open web page with release notes.
         $parameters = @{
             FilePath     = "Chrome.exe"
             ArgumentList = 'https://www.microsoft.com/en-us/wdsi/definitions/antimalware-definition-release-notes'
         }
         $parameters
         start-process @parameters -Verbose
-        # start-process chrome.exe -ArgumentList 'https://www.microsoft.com/en-us/wdsi/definitions/antimalware-definition-release-notes'
     }
-    else { write-error ("File '$file' was not found.") }
+    else {
+        write-error ("File '$file' was not found.") 
+    }
     if (test-path $file) { remove-item $file -Verbose }
     
-    Start-Sleep -Seconds 30 -Verbose
-    Write-Warning "Leaving function $($MyInvocation.MyCommand.Name):"
-    exit
-}#fjApplyVirusDefinition
+    Start-Sleep -Seconds $Seconds -Verbose
+    Write-Warning "Leaving $PSCommandPath"
+    return $true
+} # fjApplyVirusDefinition
 
-fjApplyVirusDefinition
+fjApplyVirusDefinition -Seconds 15

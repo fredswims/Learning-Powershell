43a873750acc22eb2528580575c0b3f16b1403ab
fredswims <fredswims@gmail.com>
2025-11-06 23:15:50 -0500

    MoreSnippets

--- MyStuff/FredsSnippets.ps1
+++ MyStuff/FredsSnippets.ps1
@@ -44,3 +44,48 @@ if ($isElevated) {
 } else {
     Write-Host "Not running as administrator"
 }
+
+
+    #Launch Quicken and monitor its exit code.
+    do {
+        #$LastExitCode = 0
+        if ($bSayit) { [Void]$oSynth.SpeakAsync(("{0}     you are invoking Quicken with {1}" -f $env:USERNAME, $Filename)) }
+
+        # Launch Quicken using file association and WAIT for it to exit.
+        $myprocess = start-process -FilePath $DestinationPath -passthru -Verbose
+        if ($Priority) {
+            write-host "PriorityClass: [$($MyProcess.PriorityClass)]"
+            write-host "BasePriority:  [$($MyProcess.BasePriority)]"
+            write-warning "Setting Quicken Process Priority to AboveNormal"
+            start-sleep -Milliseconds 500; 
+            $MyProcess.PriorityClass = [System.Diagnostics.ProcessPriorityClass]::AboveNormal
+        }
+        start-sleep -Milliseconds 500; 
+        "PriorityClass: $($MyProcess.PriorityClass)"
+        "BasePriority:  $($MyProcess.BasePriority)"
+        Write-Warning ("{0}{1}DO NOT CLOSE THIS WINDOW DO NOT CLOSE THIS WINDOW DO NOT CLOSE THIS WINDOW{2}" -f $PSStyle.Blink, $PSStyle.foreground.red, $psstyle.Reset)
+        $Myprocess.WaitForExit()
+        # Waiting for Quicken to exit
+        $exitCode = $myprocess.ExitCode
+        # Read-Host -Prompt "Last Exit Code  $($ExitCode)"
+
+        "You worked with Quicken this long."
+        New-TimeSpan -Start $myprocess.StartTime -End $myprocess.ExitTime # references $myprocess object.
+        write-host "LastExitCode $ExitCode"
+        if ($ExitCode -ne 0) {
+            [console]::beep($ToneBad, $ToneDuration) #works for all versions
+            $Sayit = "Oops, Quicken stopped with code $ExitCode Do you want to restart Quicken"
+            if ($bSayit) { [Void]$oSynth.SpeakAsync($SayIt) }
+            write-host -ForegroundColor "Red" $SayIt
+            Do { $MyResponse = Read-Host "$SayIt [Y] Yes  [N]  No" }
+            until ($MyResponse -like 'y*' -or $MyResponse -like 'n*')
+            if ($MyResponse -like "n*") { $ExitCode = 0 }
+        }
+    } until ($ExitCode -eq 0)
+
+Start-Process notepad -PassThru | Tee-Object -Variable myProcess
+Start-Sleep -Seconds 2
+$procDotNet = [System.Diagnostics.Process]::GetProcessById($myProcess.Id)
+        $procDotNet = [System.Diagnostics.Process]::GetProcessById($MyProcess.Id)
+        $procDotNet.PriorityClass
+        $procDotNet.BasePriority

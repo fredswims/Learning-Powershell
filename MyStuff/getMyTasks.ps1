02ec72a2c5bfb3967b1bd7aa4c2866f367cacf95
fredswims <fredswims@gmail.com>
2025-11-15 02:28:29 -0500

    rename

---
+++ MyStuff/getMyTasks.ps1
@@ -0,0 +1,36 @@
+function Get-MyTasks {
+    [CmdletBinding()]
+    param ()
+    Write-Warning "In script [$($PSCommandPath)]: [$(get-date -Format "dddd yyyy-MM-dd hh:mm:ss K")]"
+    Write-Warning "In function [$($MyInvocation.MyCommand.Name)]: [$(get-date -Format "dddd yyyy-MM-dd hh:mm:ss K")]"
+    Write-host "Getting current user identity..."
+    $identity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $domain, $user = $identity.Name -split '\\'
+
+    $results = @()
+
+    $processes = Get-CimInstance -ClassName Win32_Process
+    write-host ("Number of Processess `n`t[{0}]" -f $processes.Count)
+
+    foreach ($proc in $processes) {
+        try {
+            $owner = Invoke-CimMethod -InputObject $proc -MethodName GetOwner -ErrorAction Stop
+            if ($owner.ReturnValue -eq 0 -and
+                $owner.User -eq $user -and
+                $owner.Domain -eq $domain) {
+
+                $results += [PSCustomObject]@{
+                    ProcessId = $proc.ProcessId
+                    Name      = $proc.Name
+                    User      = "$($owner.Domain)\$($owner.User)"
+                }
+            }
+        } catch {
+            Write-host "In Catch Block `n`tSkipped $($proc.Name) (PID $($proc.ProcessId)): $_"
+            continue
+        }
+    }
+
+    return $results
+}
+Get-MyTasks

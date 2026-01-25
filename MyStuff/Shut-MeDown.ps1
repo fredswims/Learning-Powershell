2fd16a201d437bb6f8b09c157e60d50602057db3
fredswims <fredswims@gmail.com>
2026-01-24 20:04:51 -0500

    "Adding Hibernate""

--- MyStuff/Shut-MeDown.ps1
+++ MyStuff/Shut-MeDown.ps1
@@ -1,4 +1,5 @@
-<# 
+<#
+        .SYNOPSIS
 Yes—Windows 11 uses a feature called Fast Startup, which performs a hybrid shutdown. 
 To force a full restart on next boot, you can either disable Fast Startup or use a command-line shutdown that bypasses it.
 
@@ -94,13 +95,54 @@ Fix Restarting on Shutdown in Windows 11: Fast Startup, NIC Power .... https://w
 How to Fix Windows 11 Restarting Instead of Shutting Down. https://allthings.how/how-to-fix-windows-11-restarting-instead-of-shutting-down/
 
 Is there a way to have Windows 11 actually shut down?. https://learn.microsoft.com/en-us/answers/questions/4043851/is-there-a-way-to-have-windows-11-actually-shut-do
-#>
+        Frees up AI memory.
+        AUTHOR:FAJ January 2026
+        REVISION HISTORY:FAJ 2026.01.17 Version 1.2.0
+
+        .DESCRIPTION
+        PURPOSE: Free-Java-AI-Memory. Stops WSAIFabricSvc and related processes when physical memory is low.
+        Optionally just stops the processes "WorkLoadsSessionManager", "WorkloadsSessionHost" and leaves the service running.
+
+        .PARAMETER Id
+        ?Unique string identifying this header instance. Used for replacement or updating by reuse.
+
+        .PARAMETER Title
+        ?The text displayed to the user as the header.
+
+        .PARAMETER Arguments
+        ?String data passed to Activation if the header itself is clicked.
+
+        .PARAMETER ActivationType
+        ?Enum specifying the activation type (defaults to Protocol).
+
+        .INPUTS
+        None. You cannot pipe input to this function.
+
+        .OUTPUTS
+        Custom Object: 
+            Mode  - what actions were taken
+            Return - ($True/$False)
+        A BurntToast Notification
+
+        .EXAMPLE
+        ?New-BTHeader -Title 'First Category'
+        Creates a header titled 'First Category' for categorizing toasts.
+
+        .EXAMPLE
+        ?New-BTHeader -Id '001' -Title 'Stack Overflow Questions' -Arguments 'http://stackoverflow.com/'
+        Creates a header with ID '001' and links activation to a URL.
+
+        .LINK
+        ?https://github.com/Windos/BurntToast/blob/main/Help/New-BTHeader.md
+    #>
+    
+
 [CmdletBinding()]
 param (
     [ArgumentCompleter({
-        'FullShutdown', 'Restart', 'ReportBootType'
+        'FullShutdown', 'Restart', 'Hibernate', 'ReportBootType'
     })]
-    [ValidateSet("FullShutdown", "Restart", "ReportBootType")]
+    [ValidateSet("FullShutdown", "Restart", "Hibernate", "ReportBootType")]
     [string]$Action = "ReportBootType",
     [switch]$AllEvents = $false    
 )
@@ -112,6 +154,10 @@ switch ($Action) {
     "Restart" {
         Write-Host "Initiating restart..."
         shutdown /r /f /t 0
+    }
+    "Hibernate" {
+        Write-Host "Initiating hibernation..."
+        shutdown /h /f /t 0
     }
     "ReportBootType" {
         Write-Host "Reporting last boot type..."

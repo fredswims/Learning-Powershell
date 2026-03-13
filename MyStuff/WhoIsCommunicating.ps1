65ecde624d54b24603a6770dd7470b511edbca61
fredswims <fredswims@gmail.com>
2026-03-12 23:21:53 -0400

    "FAJ 2026-03-12"

--- MyStuff/WhoIsCommunicating.ps1
+++ MyStuff/WhoIsCommunicating.ps1
@@ -11,40 +11,45 @@ write-warning ("[$(Get-Date -Format "dddd MM/dd/yyyy HH:mm:ss K")]`n`t [$PSComma
 #region Who Am I
 Write-Warning (
     "`n`t[$(Get-Date -Format 'dddd MM/dd/yyyy HH:mm:ss K')]"+
-    "`n`tIn function [$($MyInvocation.MyCommand.Name)]: Show Internet Traffic on port 443 " +
-    "`n`tIn script   [$PSCommandPath]" 
+    # "`n`tIn function [$($MyInvocation.MyCommand.Name)] " +
+    "`n`tIn script   [$PSCommandPath]: " + 
+    "`n`tShow Internet Traffic on port 443 " 
 )
 #endregion
 $Process = @{
     Name       = 'Process'
     Expression = {
         # return process path
-        (Get-Process -Id $_.OwningProcess -ErrorAction SilentlyContinue).Path
-       
+        split-path -leaf -path $((Get-Process -Id $_.OwningProcess -ErrorAction SilentlyContinue).Path).trim()
     }
 }
 
+<# 
 $ProcessSplat = @{
     Name       = 'Process'
     Expression = {
         # return process path
         # (Get-Process -Id $_.OwningProcess -ErrorAction SilentlyContinue).Path
         $path = ""
-        $path=(Get-Process -Id $_.OwningProcess -ErrorAction SilentlyContinue).Path
+        $path=split-path -leaf -path $((Get-Process -Id $_.OwningProcess -ErrorAction SilentlyContinue).Path)
         if($path) {$path} else {"PID: $($_.OwningProcess)"}
 
     }
 }
+#> 
 
 $IpOwner = @{
     Name       = 'RemoteAuthority'
     Expression = {
-        ($ip = $_.RemoteAddress)
+        $ip = $_.RemoteAddress
         $info = Invoke-RestMethod -Uri "http://ipinfo.io/$ip/json"
-        '{0} ({1})' -f $info.Org, $info.City
+        # $Info.Org consists of the ASN (Autonomous System Number) and the name of the organization that owns the IP address.
+        # e.g. AS15169 Google LLC 
+        # e.g. Mountain View
+        '{0} ({1})' -f $info.Org, $info.City 
+
     }
 }
-
 
 
 # get all connections to port 443 (HTTPS)
@@ -52,4 +57,13 @@ Get-NetTCPConnection -RemotePort 443 -State Established |
 # where there is a remote address
 Where-Object RemoteAddress |
 # and resolve IP and Process ID
-Select-Object -Property $IPOwner, RemoteAddress, OwningProcess, $Process
+# Select-Object -Property $IPOwner, RemoteAddress, OwningProcess, $Process
+
+# reorder output and narrow OwningProcess by use a calculated Splat.
+Select-Object -Property $IPOwner, RemoteAddress, $Process, @{ N ='PID'; E = {$_.OwningProcess}}  
+
+<# 
+$NetConx=Get-NetTCPConnection -RemotePort 443 -State Established
+$RealNetConx=$NetConx| Where-Object RemoteAddress
+$RealNetConx|Select-Object -Property $IPOwner, RemoteAddress, $Process, @{ N ='PID'; E = {$_.OwningProcess}} 
+#>
